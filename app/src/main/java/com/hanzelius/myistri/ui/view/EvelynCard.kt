30b0093f43ab1fa85716dd96a3b6ef903b857604
JesCe303 <jasonchristopher152@gmail.com>
2025-10-22 14:16:54 +0700

    Aku adalah anak gembala

--- app/src/main/java/com/hanzelius/myistri/ui/view/EvelynCard.kt
+++ app/src/main/java/com/hanzelius/myistri/ui/view/EvelynCard.kt
@@ -1,11 +1,30 @@
 package com.hanzelius.myistri.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hanzelius.myistri.R
 
 @Composable
 fun EvelynCard() {
 
+    Image(
+        painter = painterResource(R.drawable.agent_evelyn_chevalier_zzz_portrait),
+        contentDescription = "Evelyn Card",
+        modifier = Modifier
+            .width(100.dp)
+            .aspectRatio(0.675f)
+            .clip(RoundedCornerShape(12.dp)),
+        contentScale = ContentScale.Fit
+    )
 }
 
 @Composable
