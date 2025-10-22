d1991095ae50f89f50554b8cbbe19ad52cd8796d
JesCe303 <jasonchristopher152@gmail.com>
2025-10-22 14:12:53 +0700

    Aku adalah anak gembala

---
+++ app/src/main/java/com/hanzelius/myistri/ui/view/EvelynCard.kt
@@ -0,0 +1,15 @@
+package com.hanzelius.myistri.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun EvelynCard() {
+
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun MovieCardPreview() {
+    EvelynCard()
+}
